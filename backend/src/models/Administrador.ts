fa5eecf77ad0d6c6ac51796e3ea4d8c1e8e5c92d
Kaylany Vitoria <gamerkaylany@gmail.com>
2025-05-14 18:28:22 -0300

    Update Administrador.ts

---
+++ backend/src/models/Administrador.ts
@@ -0,0 +1,27 @@
+import mongoose, {Schema, Document} from 'mongoose'; 
+
+export interface IAdministrador extends Document{
+    nome: string;
+    email: string;
+    matricula: string;
+}
+
+const AdministradorSchema = new Schema<IAdministrador>({
+    nome: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    matricula:{
+        type: String,
+        required: true,
+        unique:true,
+    },
+});
+
+const AdministradorModel = mongoose.model<IAdministrador>('Administrador', AdministradorSchema);
+export default AdministradorModel;
