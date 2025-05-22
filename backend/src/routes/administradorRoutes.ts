9e6c0fe49bbdb9430187caa95ee294c82de28d23
Kaylany Vitoria <gamerkaylany@gmail.com>
2025-05-22 10:32:00 -0300

    Rotas do  administrador

---
+++ backend/src/routes/administradorRoutes.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response, RequestHandler } from "express";
+import {buscarTodosAdministradores,
+    buscarAdministradorPorId,
+    buscarAdministradorPorMatricula,
+    criarAdministrador,
+    atualizarAdministrador,
+    removerAdministrador} from "../controllers/AdministradorController";
+
+const router = express.Router();
+
+router.get('/', buscarTodosAdministradores as RequestHandler);
+router.get('/matricula/:matricula', buscarAdministradorPorMatricula as RequestHandler);
+router.get('/:id', buscarAdministradorPorId as RequestHandler);
+router.post('/', criarAdministrador as RequestHandler);
+router.put('/:id', atualizarAdministrador as RequestHandler);
+router.delete('/:id', removerAdministrador as RequestHandler);
+
+export default router;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import alunoRoutes from "./alunoRoutes";
 import carteirinhaRoutes from "./carteirinhaRoutes";
+import administradorRoutes from "./administradorRoutes";
 
 const router = express.Router();
 
 router.use('/alunos', alunoRoutes);
 router.use('/carteirinhas', carteirinhaRoutes);
+router.use('/administrador', administradorRoutes);
 
 export default router;
